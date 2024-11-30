0037f096eba16d6ca44d68d5afe598c33046312c
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-11-30 15:54:04 +0100

    LogIn component

--- authentication/src/App.js
+++ authentication/src/App.js
@@ -3,6 +3,8 @@ import Signup from "./components/Signup.js"
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "./contexts/AuthContext.js"
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Dashboard from "./components/Dashboard.js"
+import Login from "./components/Login.js"
 
 function App() {
 
@@ -14,7 +16,9 @@ function App() {
             <Router>
               <AuthProvider>
                 <Routes>
+                  <Route exact path="/" element={<Dashboard />}/>
                   <Route path='/signup' element={<Signup />}/>
+                  <Route path="/login" element={<Login />}/>
                 </Routes>
               </AuthProvider>
             </Router>
--- authentication/src/components/Login.js
+++ authentication/src/components/Login.js
@@ -3,23 +3,18 @@ import { Card, Form, Button, Alert } from 'react-bootstrap'
 import { useAuth } from "../contexts/AuthContext"
 import { Link } from 'react-router-dom'
 
-export default function Signup() {
+export default function Login() {
 
     const emailRef = useRef()
     const passwordRef = useRef()
-    const passwordConfirmRef = useRef()
-    const { signup} = useAuth()
+    const { signup } = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
 
     async function handleSubmit(e) {
         e.preventDefault()
 
-        if(passwordRef.current.value !== passwordConfirmRef.current.value){
-            return setError("Passwords do not match")
-        }
-
-        try{
+        try {
             setError('')
             setLoading(true)
             await signup(emailRef.current.value, passwordRef.current.value)
@@ -34,7 +29,7 @@ export default function Signup() {
         <>
             <Card>
                 <Card.Body>
-                    <h2 className='text-center mb-4'>Sign up</h2>
+                    <h2 className='text-center mb-4'>Log In</h2>
                     {/* {JSON.stringify(currentUser.email)} */}
                     {error && <Alert variant="danger">{error}</Alert>}
                     <Form onSubmit={handleSubmit}>
@@ -46,16 +41,12 @@ export default function Signup() {
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" ref={passwordRef} required />
                         </Form.Group>
-                        <Form.Group id="password-confirm">
-                            <Form.Label>Password Confirmation</Form.Label>
-                            <Form.Control type="password" ref={passwordConfirmRef} required />
-                        </Form.Group>
-                        <Button disabled={loading} type="submit" className='w-100'>Sign Up</Button>
+                        <Button disabled={loading} type="submit" className='w-100'>Log In</Button>
                     </Form>
                 </Card.Body>
             </Card>
             <div className='w-100 text-center mt-2'>
-                Already have an account? <Link to="/login">Log In</Link>
+                Need an account? <Link to="/signup">Sign Up</Link>
             </div>
         </>
     )
