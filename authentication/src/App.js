f8e35255b2b303304977760ca91ec975724fbf5e
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-11-30 15:46:53 +0100

    Router setup

--- authentication/src/App.js
+++ authentication/src/App.js
@@ -2,19 +2,24 @@
 import Signup from "./components/Signup.js"
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "./contexts/AuthContext.js"
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <AuthProvider>
         <Container className="d-flex align-items-center justify-content-center"
           style={{ minHeight: '100vh' }}>
           <div className="w-100" style={{ maxWidth: "400px" }}>
-            <Signup />
+            <Router>
+              <AuthProvider>
+                <Routes>
+                  <Route path='/signup' element={<Signup />}/>
+                </Routes>
+              </AuthProvider>
+            </Router>
           </div>
         </Container>
-      </AuthProvider>
     </>
   )
 }
