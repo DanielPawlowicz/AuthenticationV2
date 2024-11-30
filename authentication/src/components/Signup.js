a2077a4b3aa81fb5790cc2089d07d3164963fa26
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-11-30 19:55:50 +0100

    Login component

--- authentication/src/components/Signup.js
+++ authentication/src/components/Signup.js
@@ -1,16 +1,17 @@
 import React, { useRef, useState } from 'react'
 import { Card, Form, Button, Alert } from 'react-bootstrap'
 import { useAuth } from "../contexts/AuthContext"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Signup() {
 
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
-    const { signup} = useAuth()
+    const { signup } = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
 
     async function handleSubmit(e) {
         e.preventDefault()
@@ -23,6 +24,7 @@ export default function Signup() {
             setError('')
             setLoading(true)
             await signup(emailRef.current.value, passwordRef.current.value)
+            navigate('/')
         } catch {
             setError('Failed to create an account')
         }
--- authentication/src/contexts/AuthContext.js
+++ authentication/src/contexts/AuthContext.js
@@ -16,6 +16,10 @@ export function AuthProvider({ children }) {
         auth.createUserWithEmailAndPassword(email, password)
     }
 
+    function login(email, password) {
+        return auth.signInWithEmailAndPassword(email, password)
+    }
+
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user)
@@ -29,6 +33,7 @@ export function AuthProvider({ children }) {
 
     const value = {
         currentUser,
+        login,
         signup
     }
 
