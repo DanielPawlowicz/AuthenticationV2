e0581bd95353532215ccd306234657c032570514
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-12-02 20:46:29 +0100

    Dashboard component

--- authentication/src/components/Dashboard.js
+++ authentication/src/components/Dashboard.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Card, Button, Alert} from 'react-bootstrap'
+import {useAuth} from '../contexts/AuthContext'
+import { Link } from 'react-router-dom'
 
 const Dashboard = () => {
+  
+  const [error, setError] = useState('')
+  const {currentUser} = useAuth()
+
+  function handleLogout() {
+
+  }
+
   return (
-    <div>Dashboard</div>
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className='text-center mb-4'>Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <div>
+            <strong>Email: </strong> {currentUser.email}
+          </div>
+          <Link to="/update-profile" className="btn btn-primary 2-100 mt-3">Update Profile</Link>
+        </Card.Body>
+      </Card>
+      <div className='w-100 text-center mt-2'>
+        <Button variant='link' onClick={handleLogout}>Log Out</Button>
+      </div>
+    </>
   )
 }
 
